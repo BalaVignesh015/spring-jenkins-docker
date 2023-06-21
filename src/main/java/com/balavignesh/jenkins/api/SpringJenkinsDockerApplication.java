753bce99c20173fbc52eb6f26f3c111d0aedeee1
Bala Subiramaniyanm <bala.vignesh@lowcodeminds.com>
2023-06-21 09:35:34 +0530

    first commit

---
+++ src/main/java/com/balavignesh/jenkins/api/SpringJenkinsDockerApplication.java
@@ -0,0 +1,21 @@
+package com.balavignesh.jenkins.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringJenkinsDockerApplication {
+
+	@GetMapping("/message")
+	public String getMessage() {
+		return "Welcome to SpringBoot Jenkins Docker Integration...";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJenkinsDockerApplication.class, args);
+	}
+
+}
